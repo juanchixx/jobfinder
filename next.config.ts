ba60f6ade3cc57c6994c9100dba5b24147ac04d7
juanchixx <juanchixx@gmail.com>
2025-10-10 13:48:05 -0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/jobfinder"
+  basePath: "",
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
