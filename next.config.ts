82a47fecafcde9fe1e096907173d4deb1a67a752
juanchixx <juanchixx@gmail.com>
2025-10-10 12:31:07 -0300

    refactor: update next.config.ts to include basePath and module exports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/jobfinder',
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
